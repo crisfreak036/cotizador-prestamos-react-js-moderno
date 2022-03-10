818ba7e02f595309288642cb94c1cb078bfe9ede
crisfreak <cristopher.sotog@utem.cl>
2022-03-09 21:52:18 -0300

    Add new component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { Fragment } from "react";
+import Header from "./components/Header"
 
 function App() {
   return (
-    <div className="App">
-      <h1>
-        Hola Mundo desde React
-      </h1>
-    </div>
+    <Fragment>
+      {/* Cada componente <Header /> es un elemento que se muestra en el HTML */}
+      <Header />
+      <Header />
+      <Header />
+      <Header />
+      <Header />
+    </Fragment>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Header() {
+
+    return(
+        <h1>Hola Mundo</h1>
+    )
+}
+
+export default Header;
