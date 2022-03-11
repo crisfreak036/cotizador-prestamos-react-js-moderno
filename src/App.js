85340714fc8010f455c905675d6e19cabd426d11
crisfreak <cristopher.sotog@utem.cl>
2022-03-10 22:23:08 -0300

    Add props to the component Header

--- src/App.js
+++ src/App.js
@@ -5,11 +5,10 @@ function App() {
   return (
     <Fragment>
       {/* Cada componente <Header /> es un elemento que se muestra en el HTML */}
-      <Header />
-      <Header />
-      <Header />
-      <Header />
-      <Header />
+      <Header
+        titulo="Cotizador de Prestamos"
+        descripcion="Utiliza el formulario y obtén una cotización"
+      />
     </Fragment>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
-function Header() {
+// function Header(props) {
+function Header({titulo, descripcion}) {
+
+    /* Entre la declaración de la función y el return
+     se puede colocar código standard de JS */
+
+    console.log(titulo, descripcion)
+    // console.log(props)
 
     return(
-        <h1>Hola Mundo</h1>
+        <Fragment>
+            {/* Dentro de las llaves, lo que hay
+            es código JS */}
+            <h1>{ titulo }</h1>
+            <p>{ descripcion }</p>
+            {/* <h1>{ props.titulo }</h1> */}
+            {/* <p>{ props.descripcion }</p> */}
+        </Fragment>
     )
 }
 
