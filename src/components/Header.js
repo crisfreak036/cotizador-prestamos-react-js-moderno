893b934071926f5e9303c6f70a59ff20eeaac4e8
crisfreak <cristopher.sotog@utem.cl>
2022-03-12 14:35:31 -0300

    Clean the code

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,16 @@
 import React, { Fragment } from 'react';
 
-// function Header(props) {
-function Header({titulo, descripcion}) {
+function Header({titulo}) {
 
     /* Entre la declaración de la función y el return
      se puede colocar código standard de JS */
-
-    console.log(titulo, descripcion)
-    // console.log(props)
 
     return(
         <Fragment>
             {/* Dentro de las llaves, lo que hay
             es código JS */}
             <h1>{ titulo }</h1>
-            <p>{ descripcion }</p>
-            {/* <h1>{ props.titulo }</h1> */}
-            {/* <p>{ props.descripcion }</p> */}
         </Fragment>
     )
 }
-
-// const Header = ({ titulo, descripcion }) => ( 
-//     <Fragment>
-//         <h1>{ titulo }</h1>
-//         <p>{ descripcion }</p>
-//     </Fragment>
-// );
-
 export default Header;
