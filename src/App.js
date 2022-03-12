a2ada445e9a11b94f78b657c0e2e1572da90aa9e
crisfreak <cristopher.sotog@utem.cl>
2022-03-12 16:31:38 -0300

    Add global states

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import Header from "./components/Header"
 import Form from "./components/Form"
 
 function App() {
+
+      // Definir el State
+      const [loan, saveLoad] = useState(0);
+      const [deadLineValue, saveDeadLine] = useState("");
+
   return (
     <Fragment>
       {/* Cada componente <Header /> es un elemento que se muestra en el HTML */}
       <Header
         titulo="Cotizador de Prestamos"
       />
-      <Form/>
+      <Form
+        loan={loan}
+        saveLoad={saveLoad}
+        deadLineValue={deadLineValue}
+        saveDeadLine={saveDeadLine}
+
+      />
     </Fragment>
   );
 }
