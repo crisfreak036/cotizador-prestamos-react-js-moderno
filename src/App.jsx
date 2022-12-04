131646fcea79e2e7f7a5c7a11d1d61e19815c7a7
crisfreak <cristopher.sotog@utem.cl>
2022-12-04 17:18:54 -0300

    [update] Primer commit v2

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
+      <h1 className="text-center text-2xl font-bold">Hola Mundo!</h1>
+    </div>
+  )
+}
+
+export default App
--- src/components/Message.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Message = () => ( 
-    <div className='mensajes'>
-        <p>Agregue una cantidad y plazo a pagar para obtener una cotización</p>
-    </div>
- );
- 
-export default Message;
