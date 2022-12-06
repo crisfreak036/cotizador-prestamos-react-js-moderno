34f55fde36e826d91d5534812c53b3288ca6f9c9
crisfreak <cristopher.sotog@utem.cl>
2022-12-06 00:06:17 -0300

    [add] botones de incremento y decremento

--- src/App.jsx
+++ src/App.jsx
@@ -13,10 +13,41 @@ function App() {
     setCantidad(Number(e.target.value))
   }
 
+  const handleDecremento = () => {
+    const valor = cantidad - STEP
+    if (valor < MIN) return alert('Cantidad no valida') 
+    setCantidad(valor)
+  }
+
+  const handleIncremento = () => {
+    const valor = cantidad + STEP
+    if (valor > MAX) return alert('Cantidad no valida') 
+    setCantidad(valor)
+  }
+
   return (
     <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
       <Header/>
 
+      <div
+        className='flex justify-between my-6'
+      >
+        <button
+          type='button'
+          className='h-10 w-10 flex items-center justify-center font-bold text-white text-2xl bg-lime-500 rounded-full hover:ring-2 hover:ring-offset-2 hover:ring-lime-500'
+          onClick={handleDecremento}
+        >
+          -
+        </button>
+        <button
+          type='button'
+          className='h-10 w-10 flex items-center justify-center font-bold text-white text-2xl bg-lime-500 rounded-full hover:ring-2 hover:ring-offset-2 hover:ring-lime-500'
+          onClick={handleIncremento}
+        >
+          +
+        </button>
+      </div>
+      
       <input 
         type="range" 
         name="" 
