35d489c78be6b73b5efc115a493ce7c0f2adb38a
crisfreak <cristopher.sotog@utem.cl>
2022-12-05 23:45:44 -0300

    [add] logica para el input range

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
+import { useState } from 'react'
 import Header from './components/Header'
 
 function App() {
+
+  const [ cantidad, setCantidad ] = useState(10000)
+  
+  const MIN = 0
+  const MAX = 20000
+  const STEP = 100
+
+  const handleChange = (e) => {
+    setCantidad(Number(e.target.value))
+  }
 
   return (
     <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
@@ -11,7 +22,19 @@ function App() {
         name="" 
         id="" 
         className='w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600 mt-5'
+        value={cantidad}
+        onChange={handleChange}
+        min={MIN}
+        max={MAX}
+        step={STEP}
       />
+
+      <p
+        className='text-center my-10 text-5xl font-extrabold text-indigo-600'
+      >
+        {cantidad}
+      </p>
+
     </div>
   )
 }
