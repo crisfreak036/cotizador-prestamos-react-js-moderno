4b047cbe209a2dd299dd15727657160be1c654f1
crisfreak <cristopher.sotog@utem.cl>
2022-12-05 23:05:56 -0300

    [update] componente principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Header from './components/Header'
+
 function App() {
 
   return (
     <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
-      <h1 className="text-center text-2xl font-bold">Hola Mundo!</h1>
+      <Header/>
     </div>
   )
 }
