cb00c5305d71b368152d6af6eb2f4b5b48e564f1
crisfreak <cristopher.sotog@utem.cl>
2022-12-05 23:15:53 -0300

    [add] input range

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,13 @@ function App() {
   return (
     <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
       <Header/>
+
+      <input 
+        type="range" 
+        name="" 
+        id="" 
+        className='w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600 mt-5'
+      />
     </div>
   )
 }
